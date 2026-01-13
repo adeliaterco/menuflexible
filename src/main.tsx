5104f3007182b60af10b99aa568b65e8f95d583d
adeliaterco <adeliarosaterco@gmail.com>
2026-01-13 16:07:47 -0300

    hdhhd

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import Layout from "./components/Layout.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <Layout>
-    <App />
-  </Layout>
+  <App />
 );
